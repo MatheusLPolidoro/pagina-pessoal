4c9861d91c53013fa57dda6c125cc46fa9118ed9
Matheus Polidoro <matheus.polidoro@senaisp.edu.br>
2021-10-05 23:55:32 -0300

    Minha página

---
+++ script.js
@@ -0,0 +1,15 @@
+/* ------- Menu lateral ------- */
+
+document.querySelector(".hamburguer").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+);
+
+/* ------- Loader ------- */
+
+$(window).on('load', function() {
+    $('#preloader .inner').fadeOut();
+    $('#preloader').delay(450).fadeOut('slow');
+    $('body').delay(450).css({ 'overflow': 'visible' });
+})
+
+
